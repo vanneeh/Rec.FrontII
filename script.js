37a1aa69fd3e87895e87da2d46764ae6b55cc5de
vanneeh <49892008+vanneeh@users.noreply.github.com>
2022-05-03 20:01:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,106 +1,67 @@
-// fetch funcionando pra salvar a lista no localStorage
-fetch('https://fakestoreapi.com/products?limit=5')
+//pega o botao pra mostrar os produtos na lista
+const btnMostrarProdutos = document.getElementById("showProducts");
+const listaDeProdutos = document.getElementById('list');
+const novaListaProdutos = document.getElementById('newList');
+
+/* let lista = JSON.parse(localStorage.getItem('items')); */
+
+novaListaProdutos.addEventListener('click', () => {
+    // fetch funcionando pra salvar a lista no localStorage
+    fetch('https://fakestoreapi.com/products?limit=5')
         .then((resposta) => resposta.json())
         .then((produtos) => {
             console.log(produtos)
-            localStorage.setItem('items', JSON.stringify([produtos]))
-            })
+            localStorage.setItem('items', JSON.stringify(produtos))
+        })
         .catch((erro) => console.log(erro))
 
-// pega o botao pra mostrar os produtos na lista
-const btnMostrarProdutos = document.getElementById("showProducts");
+});
 
 const mostrarProdutos = () => {
     let lista = JSON.parse(localStorage.getItem('items'));
-    lista.forEach(function (produtos) {
-        document.querySelector("#list").innerHTML +=
-            `
-             <li class="item">
-             <div class="descricao">
-               <p class="nome">${produtos.title}</p>
-             </div>
-             </li>
-            `;
+    listaDeProdutos.innerHTML = "";
+    lista.forEach(function (el) {
+        const item = document.createElement('div');
+        const conteudo = `
+        <li class="item">
+        <p class="title">${el.title}</p>
+        </li>
+        `;
+        item.classList.add('produto');
+        item.id = el.id;
+        item.innerHTML = conteudo;
+
+        item.appendChild(BotaoDeleta());
+
+        listaDeProdutos.appendChild(item);
     });
 };
 
-
-
-
-//const produtos = () => {
-    //fetch('https://fakestoreapi.com/products?limit=5')
-       // .then((resposta) => resposta.json())
-       // .then((json) => console.log(json))
-/* .then((produtos) => {
-     produtos.forEach(function (produtos) {
-         document.querySelector("#list").innerHTML +=
-             `
-              <li class="item">
-              <div class="descricao">
-                <p class="nome">${produtos.title}</p>
-              </div>
-              </li>
-             `;
-    // });
-// });
-//};
-
-/*
-
-produtos.forEach(function (produtos) {
- document.querySelector("#list").innerHTML +=
-     `
-      <li class="item">
-      <div class="descricao">
-        <p class="nome">${produtos.title}</p>
-      </div>
-      </li>
-     `;
-// });
-//});
-*/
-
-// listener pra quando clicar no botão chamar a função que mostra na tela
-btnMostrarProdutos.addEventListener('click', (mostrarProdutos));
-
+//botao de deletar
 const BotaoDeleta = () => {
     const BotaoDeleta = document.createElement('button');
-    //cria botão
     BotaoDeleta.classList.add('delete-button');
-    BotaoDeleta.innerText='Deletar';
-    //texto do botao
+    BotaoDeleta.innerText = 'Deletar';
     BotaoDeleta.addEventListener('click', removerItem);
-    //escutador de evento
     return BotaoDeleta;
-    //retorna o botão
 };
 
-const removerItem = (event) =>{
-    const BotaoDeleta = event.target;
-    //evento ocorre onde o cursor selecionar
-    const itemDeletado = BotaoDeleta.parentElement;
-    // o evento ecoará para o elemento pai
-    itemDeletado.remove();
-    //método de remoção
-}
+//função de remover o item da lista
+const removerItem = (event) => {
+    let lista = JSON.parse(localStorage.getItem('items'));
+    localStorage.removeItem('items');
+    let removerItem = event.target;
+    let itemRemovido = removerItem.parentElement.id;
+
+    console.log(itemRemovido);
+   /* console.log(nova);
+    nova = lista.filter((item) => item.id !== itemRemovido);
+    console.log(nova); */
+    localStorage.setItem('items', JSON.stringify(lista.filter((item) => item.id !== itemRemovido)));
+
+    mostrarProdutos();
+};
 
 
-
-/* 
-const item = document.createElement('div');
-const listaProdutos = document.getElementById('list');
-item.classList.add('produto');
-
-let lista = localStorage.getItem('items');
-const conteudo =`
-    <div class="item">
-    <div class="info">
-        <h3>${lista.id}</h3>
-        <p>${lista.title} </p>
-    </div>
-    </div>`;
-
-item.innerHTML=conteudo;
-item.appendChild(BotaoDeleta());
-
-listaProdutos.appendChild(item); */
+// listener pra quando clicar no botão chamar a função que mostra na tela
+btnMostrarProdutos.addEventListener('click', mostrarProdutos);
